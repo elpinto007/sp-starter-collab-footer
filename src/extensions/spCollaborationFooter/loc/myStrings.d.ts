8c2755cd0bf27c9fd8fc9295679d87e87a7284f5
Krishant Lodhia <krishant007@gmail.com>
2019-01-30 12:21:17 -0800

    Initialize Application Customizer

---
+++ src/extensions/spCollaborationFooter/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ISpCollaborationFooterApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'SpCollaborationFooterApplicationCustomizerStrings' {
+  const strings: ISpCollaborationFooterApplicationCustomizerStrings;
+  export = strings;
+}
